c2df7b67de371822d8380175f077ab1d9dee50db
danwook <k.danwoo91@ibksystem.co.kr>
2021-08-26 17:40:55 +0900

    SpringBoot JPA 기본설정 및 Test Junit4 설정 추가

---
+++ src/test/java/com/kdanwoo/ormjpademo/repository/MemberRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.kdanwoo.ormjpademo.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void save() {
+
+    }
+
+    @Test
+    public void find() {
+    }
+}
