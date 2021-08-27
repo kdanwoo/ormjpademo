71a6fb1bb60797044266fad2eba325a2a72e70ed
danwook <k.danwoo91@ibksystem.co.kr>
2021-08-27 18:49:54 +0900

    Test unit 설정

--- src/test/java/com/kdanwoo/ormjpademo/repository/MemberRepositoryTest.java
+++ src/test/java/com/kdanwoo/ormjpademo/repository/MemberRepositoryTest.java
@@ -1,12 +1,13 @@
 package com.kdanwoo.ormjpademo.repository;
 
+import com.kdanwoo.ormjpademo.entity.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,11 +17,19 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    public void save() {
+    @Transactional //Spring에서는 @Transactional 어노테이션이 테스트 케이스에 있으면 테스트 마치고 rollback 해버림.
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
 
-    }
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
 
-    @Test
-    public void find() {
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
     }
 }
