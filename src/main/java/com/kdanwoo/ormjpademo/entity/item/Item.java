2397e7afe37221a46548f8c5b2f1166e6450353c
danwook <k.danwoo91@ibksystem.co.kr>
2021-09-03 23:40:04 +0900

    상품 엔티티 개발 -> 비즈니스 로직 추가 NotEnoughStockException 예외 개발 처리

--- src/main/java/com/kdanwoo/ormjpademo/entity/item/Item.java
+++ src/main/java/com/kdanwoo/ormjpademo/entity/item/Item.java
@@ -1,6 +1,7 @@
 package com.kdanwoo.ormjpademo.entity.item;
 
 import com.kdanwoo.ormjpademo.entity.Category;
+import com.kdanwoo.ormjpademo.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,6 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -28,4 +28,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스 로직==//
+    public void addStockQuantity(int quantity){
+        this.stockQuantity += quantity; //재고수량 증가로직
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
