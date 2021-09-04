c8601386dd78dc12b7f09efc3bb071ae09e54ad0
kdanwoo <k.danwoo91@gmail.com>
2021-09-04 14:06:12 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/com/kdanwoo/ormjpademo/entity/Order.java
+++ src/main/java/com/kdanwoo/ormjpademo/entity/Order.java
@@ -1,8 +1,10 @@
 package com.kdanwoo.ormjpademo.entity;
 
+import com.kdanwoo.ormjpademo.enums.DeliveryStatus;
 import com.kdanwoo.ormjpademo.enums.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Delegate;
 import lombok.extern.java.Log;
 
 import javax.persistence.*;
@@ -51,6 +53,48 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /**
+     * 생성 메서드
+     * */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems ){
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+
+        return  order;
+    }
+
+    //비즈니스 로직 추가
+    /**
+     * 주문 취소
+     * */
+    public void cancle(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setOrderStatus(OrderStatus.CANCEL);
+
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**전체 주문 가격 조회*/
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 
 
