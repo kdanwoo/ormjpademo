48aed851f5bd612f81afb8f85097fa0e369eaaa5
kdanwoo <k.danwoo91@gmail.com>
2021-09-04 14:15:16 +0900

    OrderRepository 개발

---
+++ src/main/java/com/kdanwoo/ormjpademo/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package com.kdanwoo.ormjpademo.repository;
+
+import com.kdanwoo.ormjpademo.entity.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    //public List<Order> findAll(OrderSearch orderSearch) { ... }
+}
