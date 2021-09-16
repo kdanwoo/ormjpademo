4cf096939a4841b252b9646242835ec5757e5c04
kdanwoo <k.danwoo91@gmail.com>
2021-09-17 00:49:39 +0900

    OrderService OrderServiceImpl

--- src/main/java/com/kdanwoo/ormjpademo/entity/item/Item.java
+++ src/main/java/com/kdanwoo/ormjpademo/entity/item/Item.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -41,5 +42,4 @@ public abstract class Item {
         }
         this.stockQuantity = restStock;
     }
-
 }
--- src/main/java/com/kdanwoo/ormjpademo/service/impl/OrderServiceImpl.java
+++ src/main/java/com/kdanwoo/ormjpademo/service/impl/OrderServiceImpl.java
@@ -59,6 +59,7 @@ public class OrderServiceImpl implements OrderService {
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
 
+
         //주문 취소
         order.cancel();
     }
