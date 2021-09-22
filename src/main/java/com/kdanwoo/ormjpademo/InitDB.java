db23eced14b293f511ac059c2c7b885d595ca00a
kdanwoo <k.danwoo91@gmail.com>
2021-09-22 18:25:09 +0900

    회원목록 조회 api 설계 및 개발

--- src/main/java/com/kdanwoo/ormjpademo/InitDB.java
+++ src/main/java/com/kdanwoo/ormjpademo/InitDB.java
@@ -17,21 +17,23 @@ public class InitDB {
     private final InitService initService;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         initService.dbInit1();
+        initService.dbInit2();
     }
 
 
     @Component
     @RequiredArgsConstructor
     @Transactional
-    static class InitService{
+    static class InitService {
 
         private final EntityManager em;
-        public void dbInit1(){
+
+        public void dbInit1() {
             Member member = new Member();
             member.setName("userA");
-            member.setAddress(new Address("서울","1","1111"));
+            member.setAddress(new Address("서울", "1", "1111"));
             em.persist(member);
 
             Book book1 = new Book();
@@ -56,6 +58,43 @@ public class InitDB {
 
             em.persist(order);
 
+        }
+
+        public void dbInit2() {
+            Member member = createMember("userB", "진주", "2", "2222");
+            em.persist(member);
+            Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+            em.persist(book1);
+            Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+            em.persist(book2);
+            Delivery delivery = createDelivery(member);
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
+            Order order = Order.createOrder(member, delivery, orderItem1,
+                    orderItem2);
+            em.persist(order);
+        }
+
+        private Member createMember(String name, String city, String street,
+                                    String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+
+        private Book createBook(String name, int price, int stockQuantity) {
+            Book book = new Book();
+            book.setName(name);
+            book.setPrice(price);
+            book.setStockQuantity(stockQuantity);
+            return book;
+        }
+
+        private Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            return delivery;
         }
     }
 }
